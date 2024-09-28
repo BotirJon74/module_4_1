39864e2d19a0232a1931cbc39c8965d5ee3fd279
BotirJon74 <31174@mail.ru>
2024-09-28 11:21:09 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,15 @@
+import fake_math as fam
+import true_math as tam
+
+
+fake_divide = fam.divide
+true_divide = tam.divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
